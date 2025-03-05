963bb6dc71cca6457804c4163150e5bba069f370
KarthikRajS32 <karthikrajit32@gmail.com>
2025-03-05 15:37:17 +0530

    score

--- js/script.js
+++ js/script.js
@@ -1,26 +1,26 @@
-function guess(){
-    var guess = document.getElementById("guess");
-    var result = document.getElementById("result")
-
-    var guessNumber = guess.value;
-
-    var random = Math.floor(Math.random() * 10) + 1;
+var guessNum = document.getElementById("guess");
+var result = document.getElementById("result");
+var score = document.getElementById("score");
+var random = Math.floor(Math.random() * 10) + 1;
+var totalScore = 10;
+function guess() {
+    var guessNumber = guessNum.value;
     
-    if(guessNumber == "")
-    {
-       alert("Please enter a number between 1 and 10.");
-    }
-    else if(guessNumber < random)
-    {
-        result.innerHTML = "My number is greater than "+guessNumber ;
-        guessNumber++;
-    }else if(guessNumber > random)
-    {
-        result.innerHTML = "My number is less than "+guessNumber ;
-        guessNumber++;
-    }else if(guessNumber == random)
-    {
-        result.innerHTML = "Well Done! You guessed my number.";
-        guessNumber = "" ;
-    }
+  if (guessNumber == "") {
+    alert("Please enter a number between 1 and 10.");
+  } else if (guessNumber < random) {
+    totalScore = totalScore - 1;
+    score.innerHTML = "Score: " + totalScore;
+    result.innerHTML = "My number is greater than " + guessNumber;
+    guessNumber++;
+  } else if (guessNumber > random) {
+    totalScore = totalScore - 1;
+    score.innerHTML = "Score: " + totalScore;
+    result.innerHTML = "My number is less than " + guessNumber;
+    guessNumber++;
+  } else if (guessNumber == random) {
+    result.innerHTML = "Well Done! You guessed my number."+"<br>"+"Your score is : "+totalScore;
+    guessNumber = "";
+    score.innerHTML = "";
+  }
 }
